ae229d9b09ae040633ab16d95a3261f1d1cb9408
simonabadcat <114427654+simonabadcat@users.noreply.github.com>
2023-02-13 01:03:06 -0300

    front-end para 'agregar comentario' se corrige redirect de response de 'send'

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -3,11 +3,11 @@ const { Comment } = require("../models");
 async function createComment(req, res) {
   await Comment.create({
     content: req.body.content,
-    author: req.body.id,
+    author: req.body.author,
     articleId: req.params.id,
   });
 
-  return res.redirect("/admin");
+  return res.redirect(`/articulos/${req.params.id}`);
 }
 
 module.exports = { createComment };
