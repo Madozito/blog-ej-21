8d91f5026f6fd7d513fde75e71a28546aed511e7
simonabadcat <114427654+simonabadcat@users.noreply.github.com>
2023-02-13 14:07:32 -0300

    algunos detalles css

--- public/js/app.js
+++ public/js/app.js
@@ -2,3 +2,8 @@
  * Colocar aquí JS "propio".
  * Notar que este código se ejecutará en el navegador.
  */
+/* function showMore() {
+    document.getElementsByClassName("texto-oculto").style.visibility = "visible";
+  }
+  
+   */
